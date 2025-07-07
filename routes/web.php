8c53eb2e04e4f6f75a053b4f603e1fdfac507d18
unknown <Qasimi@DESKTOP-2C4C5SR.topzone.com>
2025-07-07 05:25:46 +0430

    Add BrandController and CategoryController with routes items ...

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\WarehouseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('items', ItemController::class);
+Route::resource('warehouses', WarehouseController::class);
+
+Route::resource('brands', BrandController::class);
+Route::resource('categories', CategoryController::class);
